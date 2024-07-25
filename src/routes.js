3add31c5a64306488145cd2eb0d2965060ad0af5
Daniil Bobrov <allodrmen@yandex.ru>
2024-07-25 10:07:24 +0300

    создан файл routes

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const routes = {
+    domain: 'http://choosealicense.dev.rusrobots.ru',
+    baseUrl: `${this.domain}/api`,
+    licensesDictionary: '/licenses/dictionary',
+    quiz: '/quiz',
+}
+
+export default routes;
